e445f8234967edba76bc2bb7b1052fa874220af1
Erwan <61506101+erwanitwish@users.noreply.github.com>
2020-06-05 11:40:59 +0200

    css nav done (#3)
    Co-authored-by: ahamel1 <ahamel.wcs44@gmail.com>

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ContainerNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NavList = styled.ul`
+  display: flex;
+`;
+
+const NavItem = styled.li`
+  list-style: none;
+  padding: 0px;
+  margin-right: 3vw;
+`;
+
+const Logo = styled.img`
+  margin-left: 3vw;
+`;
 
 export default function Navigation() {
   return (
     <>
-      <nav>
-        <img src="#" alt="logo" />
-        <ul>
-          <li>Entreprise</li>
-          <li>Particulier</li>
-          <li>Presse</li>
-          <li>Forope</li>
-        </ul>
-      </nav>
+      <ContainerNav>
+        <Logo src="#" alt="logo" />
+        <NavList>
+          <NavItem>Entreprise</NavItem>
+          <NavItem>Particulier</NavItem>
+          <NavItem>Presse</NavItem>
+          <NavItem>Forope</NavItem>
+        </NavList>
+      </ContainerNav>
     </>
   );
 }
