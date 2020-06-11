9b1c0c56e80074b35c168f16cc7f2f4317ad2a55
Alice Hamel <61732705+ahamel1@users.noreply.github.com>
2020-06-11 10:56:08 +0200

    Bandeau (#5)
    * css nav done
    
    * css bandeau
    
    Co-authored-by: ahamel1 <ahamel.wcs44@gmail.com>

--- src/components/bandeauTitre.jsx
+++ src/components/bandeauTitre.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const ContainerBandeau = styled.div`
+  display: flex;
+  border: 2px solid black;
+  height: 50vh;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TitlesBandeau = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const SubtitleBandeau = styled.p`
+  width: 40%;
+  text-align: left;
+  align-self: flex-end;
+  margin: 0;
+`;
+
+const TitleBandeau = styled.h1`
+  font-size: 5rem;
+  margin-bottom: 1vw;
+  text-decoration: underline;
+`;
 export default function BandeauTitre() {
   return (
-    <div>
-      <h1>Forope.</h1>
-      <p>Lorem ipsum dolor sit amet, consecteconsecte- tuer adipiscing elit,</p>
-    </div>
+    <ContainerBandeau>
+      <TitlesBandeau>
+        <TitleBandeau>Forope.</TitleBandeau>
+        <SubtitleBandeau>
+          Lorem ipsum dolor sit amet, consecteconsecte- tuer adipiscing elit,
+        </SubtitleBandeau>
+      </TitlesBandeau>
+    </ContainerBandeau>
   );
 }
