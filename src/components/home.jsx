16af3706c81f95df60d1d6d762de391feeb392d9
ho823 <amelyne.ho@hotmail.fr>
2020-06-16 09:22:43 +0200

    Replace AllTestimonies before Footer in home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -13,8 +13,8 @@ export default function Home() {
       <BandeauTitre />
       <Questionaire />
       <AllArticles />
-      <Footer />
       <AllTestimonies />
+      <Footer />
     </>
   );
 }
